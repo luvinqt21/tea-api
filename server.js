797dd4dac879bbee9c20cabe04a25d8d8ee7834d
Ingrid Salgado <luvinqt21@gmail.com>
2022-06-28 21:17:18 -0400

    my own drinking tea api

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const PORT = 8000
 app.use(cors())
 
 const tea = {
-    'oolong':{
-        'type': 'black',
-        'origin': 'Yo mommas HOuse',
-        'waterTemp': 200,
-        'steepTimeSeconds': 180,
+    'oolong tea': {
+        'type': 'true tea',
+        'origin': 'Fujian, Southeastern coastal province of China',
+        'waterTemp': 184,
+        'steepTimeSeconds': '10 seconds to 8 minutes',
         'caffinated': true,
-        'flavor': 'delicious'
+        'flavor': 'wide variety of flavours and colours'
     },
-    'matcha':{
+    'matcha tea': {
         'type': 'green',
         'origin': 'Yo mommas HOuse',
         'waterTemp': 200,
@@ -22,7 +22,31 @@ const tea = {
         'caffinated': false,
         'flavor': 'delicious'
     },
-    'unknown':{
+    'black tea': {
+        'type': 'true tea',
+        'origin': 'China, and India',
+        'waterTemp': 184,
+        'steepTimeSeconds': '10 seconds to 8 minutes',
+        'caffinated': true,
+        'flavor': 'strong, bold, full body'
+    },
+    'green tea': {
+        'type': 'true tea',
+        'origin': 'China, and Japan',
+        'waterTemp': 184,
+        'steepTimeSeconds': '10 seconds to 8 minutes',
+        'caffinated': true,
+        'flavor': 'lighter, milder taste, with some hints of sweetness'
+    },
+    'white tea': {
+        'type': 'true tea',
+        'origin': 'China, and Japan',
+        'waterTemp': 184,
+        'steepTimeSeconds': '10 seconds to 8 minutes',
+        'caffinated': true,
+        'flavor': 'light with mild but crisp flavour'
+    },
+    'unknown': {
         'type': 'unknown',
         'origin': 'unknown',
         'waterTemp': 0,
@@ -32,19 +56,19 @@ const tea = {
     }
 }
 
-app.get('/', (request,response)=>{
+app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:name', (request,response)=>{
+app.get('/api/:name', (request, response) => {
     const teaName = request.params.name.toLowerCase()
-    if( tea[teaName] ){
-       response.json(tea[teaName]) 
-    }else{
+    if (tea[teaName]) {
+        response.json(tea[teaName])
+    } else {
         response.json(tea['unknown'])
     }
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is running on port ${PORT}! Betta Go Catch It!`)
 })
